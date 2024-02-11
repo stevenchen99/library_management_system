ca6ccbd300dc52da497355acd47bdda8f223d1e6
KWYK <kwykyaw.99@gmail.com>
2024-02-11 12:05:46 +0800

    Add Page Transitions

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 import Navbar from '@/components/Navbar';
+import './styles.css';
 
 export default function Layout() {
+  const location = useLocation();
+
   return (
     <div>
       <Navbar />
-      <div className='max-w-6xl mx-auto p-3'>
-        <Outlet />
-      </div>
+      <SwitchTransition>
+        <CSSTransition timeout={200} classNames='fade' key={location.pathname}>
+          <div className='max-w-6xl mx-auto p-3'>
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 }
