adbfe05bedc07f05bcc893cb3896e2903eb72350
KWYK <kwykyaw.99@gmail.com>
2024-02-16 15:56:12 +0800

    UseReducer for UseTheme

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,9 +1,24 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 let ThemeContext = createContext();
 
+let ThemeReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE_THEME':
+      return { ...state, theme: action.payload };
+    default:
+      return state;
+  }
+};
+
 let ThemeContextProvider = ({ children }) => {
-  return <ThemeContext.Provider value={{ theme: 'dark' }}>{children}</ThemeContext.Provider>;
+  let [state, dispatch] = useReducer(ThemeReducer, { theme: 'light' });
+
+  let changeTheme = () => {
+    dispatch({ type: 'CHANGE_THEME', payload: 'dark' });
+  };
+
+  return <ThemeContext.Provider value={{ ...state, changeTheme }}>{children}</ThemeContext.Provider>;
 };
 
 export { ThemeContext, ThemeContextProvider };
